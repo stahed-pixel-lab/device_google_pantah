9c94cb2d758c7a9998869b182138c71c96c9325e
sm6150-dreams <rn10prodev@gmail.com>
2026-03-19 17:08:46 +0100

    pantah: inherit add basic call recorder support
    https://github.com/stahed-pixel-lab/vendor_bcr
    
    Signed-off-by: sm6150-dreams <rn10prodev@gmail.com>

--- alpha_cheetah.mk
+++ alpha_cheetah.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 # Face unlock 
 $(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
 
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # Camera
 $(call inherit-product-if-exists, vendor/google/camera/config.mk)
 
