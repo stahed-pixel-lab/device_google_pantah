5d17df4c8ae33958c31caeee95b08dac4fdd1cb8
sm6150-dreams <rn10prodev@gmail.com>
2026-04-26 10:34:50 +0200

    pantah: alphadroid bringup
    Signed-off-by: sm6150-dreams <rn10prodev@gmail.com>

---
+++ alpha_panther.mk
@@ -0,0 +1,73 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
+
+TARGET_IS_PIXEL_7 := true
+TARGET_IS_PIXEL := true
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build config
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# Launcher
+TARGET_INCLUDE_LAWNCHAIR := false
+
+# (valid only for GAPPS builds)
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+WITH_ADB_INSECURE := true
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Javi
+WITH_GMS := true
+WITH_PIXEL_OVERLAYS := true
+
+# Inherit device configuration
+DEVICE_CODENAME := panther
+DEVICE_PATH := device/google/pantah
+VENDOR_PATH := vendor/google/panther
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 7
+PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="panther-user 16 BP4A.251205.006 14401865 release-keys" \
+    BuildFingerprint=google/panther/panther:16/BP4A.251205.006/14401865:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
